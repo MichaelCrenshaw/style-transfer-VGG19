72a981e1358c0504bfde5bf389c5c7511c99d2ef
MichaelCrenshaw <mikemcren@aol.com>
2020-09-07 21:51:33 -0600

    Luminance only transfer

--- Code/stylize-luminance.py
+++ Code/stylize-luminance.py
@@ -47,22 +47,6 @@ def load_img(path_to_img):
     img = img[tf.newaxis, :]
     return img
 
-def load_img_colored(path_to_img):
-    max_dim = dim_limit
-    img = tf.io.read_file(path_to_img)
-    img = tf.image.decode_image(img, channels=3)
-    img = tf.image.convert_image_dtype(img, tf.float32)
-
-    shape = tf.cast(tf.shape(img)[:-1], tf.float32)
-    long_dim = max(shape)
-    scale = max_dim / long_dim
-
-    new_shape = tf.cast(shape * scale, tf.int32)
-
-    img = tf.image.resize(img, new_shape)
-    img = img[tf.newaxis, :]
-    return img
-
 
 def imshow(image, title=None):
     if len(image.shape) > 3:
@@ -343,6 +327,6 @@ file_name = 'stylized-image.png'
 
 styled_image = tensor_to_image(image)
 content_color = PIL.Image.open(content_path)
-stylized_image = style_image.paste(content_image, "L")
+stylized_image = style_image.paste(content_color, "L")
 
 stylized_image.save(file_name)
